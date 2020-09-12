2c7ae8bb27ea5bee4b0d4f7fa56ad511ecbb2a79
Jorge Luis Alfonso Chavez <jlalfonso21@gmail.com>
2020-09-12 07:51:37 -0400

    First version

---
+++ precache-manifest.81265ddccd9ff90269a8f6798975e263.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90d941a5a9a3c38f6d8a3bc149d0000f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "920f43d0030bf26bde86",
+    "url": "/static/css/2.9497d81f.chunk.css"
+  },
+  {
+    "revision": "744a30a6d400f334e624",
+    "url": "/static/css/main.255afb35.chunk.css"
+  },
+  {
+    "revision": "920f43d0030bf26bde86",
+    "url": "/static/js/2.7208df13.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.7208df13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "744a30a6d400f334e624",
+    "url": "/static/js/main.f1e743ca.chunk.js"
+  },
+  {
+    "revision": "d3124b27bbb5b4089688",
+    "url": "/static/js/runtime-main.7974d308.js"
+  },
+  {
+    "revision": "83a30f228da250b1560f14aff5c235da",
+    "url": "/static/media/Jesus.83a30f22.png"
+  },
+  {
+    "revision": "1d6a0ee65efffb715240a98ef7764b76",
+    "url": "/static/media/Sistemas.1d6a0ee6.jpg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "fbf33d7ec45d4ca634aba39444a9d729",
+    "url": "/static/media/iOSyAndroid.fbf33d7e.png"
+  }
+]);
